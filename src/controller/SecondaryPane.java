119d59a9727ffcda03b8b6b51f0e0cef641adad5
DrWursterich <31037782+DrWursterich@users.noreply.github.com>
2018-10-07 00:48:45 +0200

    Implemented Controller for SecondaryPane

---
+++ src/controller/SecondaryPane.java
@@ -0,0 +1,74 @@
+package controller;
+
+import application.Main;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class SecondaryPane {
+	@FXML
+	private GridPane root;
+	@FXML
+	private Button BackwardsButton;
+	@FXML
+	private Button PauseButton;
+	@FXML
+	private Button ForwardsButton;
+	@FXML
+	private Button SettingsButton;
+	@FXML
+	private Button MoveButton;
+	@FXML
+	private Button ExitButton;
+	@FXML
+	private Button StartNewButton;
+	@FXML
+	private Button RestartButton;
+	@FXML
+	private Button ModeButton;
+
+	public SecondaryPane() {}
+
+	@FXML
+	public void backwards() {
+		Main.getInstance().getTimer().backwards();
+	}
+
+	@FXML
+	public void pause() {
+		Main.getInstance().getTimer().pause();
+	}
+
+	@FXML
+	public void forwards() {
+		Main.getInstance().getTimer().forwards();
+	}
+
+	@FXML
+	public void settings() {
+	}
+
+	@FXML
+	public void move() {
+	}
+
+	@FXML
+	public void exit() {
+		Platform.exit();
+	}
+
+	@FXML
+	public void startNew() {
+		this.restart();
+	}
+
+	@FXML
+	public void restart() {
+		Main.getInstance().getTimer().restart();
+	}
+
+	@FXML
+	public void chooseMode() {
+	}
+}
